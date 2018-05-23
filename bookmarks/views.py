152c3340e6ff3f2f2b1a2baf1d0b377eff991571
Aaron Burk <aarondburk@gmail.com>
2018-05-23 14:41:42 -0500

    conditional rendering working on template

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Bookmark
 
 def index(request):
-    context = {}
+    context = {
+        'bookmarks': Bookmark.objects.all(),
+    }
 
     return render(request, 'bookmarks/index.html', context)
