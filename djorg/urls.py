31ff5ee775ea703545e477667ac425bb09638581
Aaron Burk <aarondburk@gmail.com>
2018-05-25 11:32:50 -0500

    prep for deploy to heroku

--- djorg/urls.py
+++ djorg/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='djorg_base.html')),
     path('bookmarks/', include('bookmarks.urls')),
     path('admin/', admin.site.urls),
+    path('signin/', TemplateView.as_view(template_name='signin/index.html'))
+
 ]
