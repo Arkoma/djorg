7bcb062bc95f121fa0c034b4b438c271f725724d
Aaron Burk <aarondburk@gmail.com>
2018-05-25 12:57:02 -0500

    change database settings to original

--- djorg/settings.py
+++ djorg/settings.py
@@ -78,7 +78,14 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES['default'] = config('DATABASE_URL')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# DATABASES['default'] = dj_database_url.config(default='DATABASE_URL')
 
 
 # Password validation
